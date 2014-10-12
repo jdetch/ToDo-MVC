890d6946ad08278754b76217612c99c6c3e66855
jdetch <jodidetch@gmail.com>
2014-10-12 14:07:04 -0400

    Todos can be edited or deleted

--- app/controllers/todo.js
+++ app/controllers/todo.js
@@ -1,9 +1,26 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
+export default Ember.ObjectController.extend({
   actions: {
       editTodo: function(){
         this.set('isEditing', true);
+      }, 
+      acceptChanges: function(){
+        // Remove isEditing property
+        this.set('isEditing', false);
+
+        //If the todo is empty, delete it
+        // otherwise save it with the new title
+        if(Ember.isEmpty(this.get('model.title'))){
+          this.send('removeTodo');
+        } else {
+            this.get('model').save();
+        }
+      },
+      removeTodo: function(){
+        var todo = this.get('model');
+        todo.deleteRecord();
+        todo.save();
       }
   }
 });
