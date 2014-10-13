43b6997c3a5f3886e932f6f86b32d5b84ec0fa4d
jdetch <jodidetch@gmail.com>
2014-10-13 11:17:18 -0400

    User can toggle ALL todos between complete/incomplete

--- app/controllers/todos.js
+++ app/controllers/todos.js
@@ -26,6 +26,17 @@ export default Ember.ArrayController.extend({
     }
   },
 
+  allAreDone: function(key, value) {
+    console.log(key + ": " + value);
+    if (value === undefined) {
+      return this.get('length') > 0 && this.isEvery('isCompleted', true);
+    } else {
+      this.setEach('isCompleted', value);
+      this.invoke('save');
+      return value;
+    }
+  }.property('@each.isCompleted'),
+
   hasCompleted: function() {  // Do we have at least one todo that has been completed?
     return this.get('completed') > 0;
   }.property('completed'),
